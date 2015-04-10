c591d9a566ebe6693ea8e6f26758534f8a3b4e6f
hubokitty <bohu.cambridge@gmail.com>
2015-04-10 16:24:29 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -17,31 +17,31 @@ require("data.table")
 require("reshape2")
 
 
-## Step 1: Read data
+# Step 1: Read data
 
-# read train data
+## read train data
 trainXdata <- read.table("train/X_train.txt")
 trainYdata <- read.table("train/y_train.txt",col.names = "label")
 trainSubjects<-read.table("train/subject_train.txt",col.names = "subject")
 
-# read test data
+## read test data
 testXdata <- read.table("test/X_test.txt")
 testYdata <- read.table("test/y_test.txt",col.names = "label")
 testSubjects<-read.table("test/subject_test.txt",col.names = "subject")
 
-# read features
+## read features
 features<-read.table("features.txt",strip.white=TRUE,stringsAsFactors=FALSE)[,2]
 
 
 
-## Step 2: Merge train and test data
+# Step 2: Merge train and test data
 Xdata<-rbind(trainXdata,testXdata)
 Ydata<-rbind(trainYdata,testYdata)
 Subjects<-rbind(trainSubjects,testSubjects)
 
 
 
-## Step 3: Extract only the measurements on the mean and standard deviation for each measurement.
+# Step 3: Extract only the measurements on the mean and standard deviation for each measurement.
 extract_features <- grepl("mean|std", features)
 extract_features_names<-features[extract_features]
 extract_Xdata <- Xdata[,extract_features]
@@ -49,19 +49,19 @@ colnames(extract_Xdata)<-extract_features_names
 
 data<-cbind(Subjects,Ydata,extract_Xdata)
 
-## Step 4: Read the activity names and use activity names to name the activities in the data set.
+# Step 4: Read the activity names and use activity names to name the activities in the data set.
 
-# read the activities
+## read the activities
 labels <-read.table("activity_labels.txt", stringsAsFactors=FALSE)
 
-# Appropriately labels the data set with descriptive activity names.
+## Appropriately labels the data set with descriptive activity names.
 data$label<-labels[data$label,2]
 
 
 
-## Step 5: Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+# Step 5: Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 tidydata<-aggregate(data[,3:ncol(data)],by=list(subject = data$subject,label = data$label), mean)
 
 
-## Step 6: Write the data to tidyData.txt file for upload
+# Step 6: Write the data to tidyData.txt file for upload
 write.table(format(tidydata, scientific=T), "tidyData.txt",row.names=F, col.names=F, quote=2)
